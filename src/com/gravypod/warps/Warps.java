a9f4c992b4ff463a80efe026e60a1418c890b1f2
Joshua Katz <gravypod@gravypod.com>
2014-05-12 22:50:20 -0400

    Correctly register the commands.

--- src/com/gravypod/warps/Warps.java
+++ src/com/gravypod/warps/Warps.java
@@ -3,6 +3,8 @@ package com.gravypod.warps;
 import com.gravypod.starmadewrapper.Server;
 import com.gravypod.starmadewrapper.plugins.Plugin;
 import com.gravypod.starmadewrapper.plugins.commands.CommandManager;
+import com.gravypod.warps.commands.AddWarpCommand;
+import com.gravypod.warps.commands.DelWarpCommand;
 import com.gravypod.warps.commands.WarpCommand;
 
 public class Warps extends Plugin {
@@ -11,12 +13,18 @@ public class Warps extends Plugin {
 	
 	@Override
 	public void onDisable(Server server) {
+	
 		CommandManager manager = getAssistant().getPluginManager().getCommandManager();
 		list = new WarpList(getDataDir());
 		manager.registerCommand("warp", new WarpCommand(list));
+		
+		manager.registerCommand("addwarp", new AddWarpCommand(list));
+		
+		manager.registerCommand("delwarp", new DelWarpCommand(list));
 	}
 	
 	public void onEnable(Server server) {
+	
 		list.save();
 	}
 	
